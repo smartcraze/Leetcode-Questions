6fdfff5aaa04bc8acf97865d23f07f9df9a79c28
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-01-27 20:26:37 +0530

    Time: 4 ms (21.62%), Space: 69.6 MB (25.67%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -6,7 +6,7 @@ public:
         int cnt = 0;
         for(int right = 0;right<nums.size();right++){
             if(nums[right]==0)cnt++;
-            while(cnt>k){
+            if(cnt>k){
                 if(nums[left]==0) cnt--;
                 left++;
             }
