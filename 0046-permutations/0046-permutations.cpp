a3c3b5900dab3a79ff642f77870bf620bb95d6ce
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-02-06 10:34:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.62%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        solve(0, nums, ans);
+        vector<int> path;
+        backtrack(0,nums,ans);
         return ans;
     }
-
-    void solve(int idx, vector<int>& nums, vector<vector<int>>& ans) {
-        if (idx == nums.size()) {
+    void backtrack(int idx , vector<int>& nums ,vector<vector<int>> &ans){
+        // BASE CASE
+        if(idx == nums.size()){
             ans.push_back(nums);
             return;
         }
 
-        for (int i = idx; i < nums.size(); i++) {
-            swap(nums[idx], nums[i]);   // choose
-            solve(idx + 1, nums, ans);  // explore
-            swap(nums[idx], nums[i]);   // un-choose (backtrack)
-        }
+        for(int i = idx;i<nums.size();i++){
+            // swap
+            swap(nums[i],nums[idx]);
+            backtrack(idx+1,nums,ans);
+            swap(nums[i],nums[idx]);
+        } 
     }
 };
