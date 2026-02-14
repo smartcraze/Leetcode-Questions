1e202b895092131220cf4d69fe70e5020092f492
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-02-14 11:17:37 +0530

    Time: 75 ms (77.9%), Space: 146.3 MB (82.23%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,13 +4,15 @@ public:
         unordered_map<int,int> mpp;
         mpp[0]= -1;
         int sum = 0;
-        int cnt = 0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(mpp.find(sum-k) != mpp.end()) cnt+=mpp[sum-k];
-            mpp[sum] = i;
+            int mod = sum%k;
+            if(mpp.find(mod)!=mpp.end()){
+                if (i - mpp[mod] >= 2) return true;
+            }else{
+                mpp[mod] = i;
+            }
         }
-        if(cnt>=2) return true;
         return false;
     }
 };
