e7b2487e01ecf143671855252b31543b27222ace
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-02-23 11:23:19 +0530

    Time: 12 ms (80.81%), Space: 34.6 MB (83.94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
         queue<pair<int,int>> q;
-        vector<vector<int>> dist(m, vector<int>(n, -1));
-        
-        // Push all 0s into queue
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 0) {
-                    q.push({i, j});
+
+        // push all 0 cells
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 0){
                     dist[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
-        
-        // Direction arrays
-        int dr[4] = {-1, 0, 1, 0};
-        int dc[4] = {0, 1, 0, -1};
-        
-        // BFS
-        while(!q.empty()) {
-            auto front = q.front();
+
+        int dx[4] = {-1,0,1,0};
+        int dy[4] = {0,1,0,-1};
+
+        while(!q.empty()){
+            auto [x,y] = q.front();
             q.pop();
-            
-            int r = front.first;
-            int c = front.second;
-            
-            for(int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && dist[nr][nc] == -1) {
-                    dist[nr][nc] = dist[r][c] + 1;
-                    q.push({nr, nc});
+
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    if(dist[nx][ny] > dist[x][y] + 1){
+                        dist[nx][ny] = dist[x][y] + 1;
+                        q.push({nx,ny});
+                    }
                 }
             }
         }
-        
+
         return dist;
     }
 };
