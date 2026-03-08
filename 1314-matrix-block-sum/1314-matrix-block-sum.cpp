1b2dfafb4526e51aacaa8bc197d9bec6ab80966f
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-03-08 14:25:21 +0530

    Time: 6 ms (29.79%), Space: 13.9 MB (45.74%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> prefix(m+1, vector<int>(n+1,0));
+
+        // build prefix sum
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                prefix[i][j] =
+                    mat[i-1][j-1]
+                    + prefix[i-1][j]
+                    + prefix[i][j-1]
+                    - prefix[i-1][j-1];
+            }
+        }
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                int r1 = max(0, i-k);
+                int c1 = max(0, j-k);
+                int r2 = min(m-1, i+k);
+                int c2 = min(n-1, j+k);
+
+                ans[i][j] =
+                    prefix[r2+1][c2+1]
+                    - prefix[r1][c2+1]
+                    - prefix[r2+1][c1]
+                    + prefix[r1][c1];
+            }
+        }
+
+        return ans;
+    }
+};
