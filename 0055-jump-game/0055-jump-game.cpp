caacea634b16a9381948e11225775304dd24a923
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-03-24 10:04:20 +0530

    Time: 0 ms (100%), Space: 52.4 MB (25.46%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxreach = 0;
+        for(int i = 0;i<n;i++){
+            if(i>maxreach)return false;
+            maxreach = max(maxreach, i+nums[i]);
+        }
+        return true;
+    }
+};
