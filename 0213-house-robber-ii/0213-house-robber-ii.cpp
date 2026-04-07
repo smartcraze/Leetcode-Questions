89404bb640e78b8424544fdc2f57f3c5fed358c9
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-04-07 10:13:18 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if(n == 1) return nums[0];
 
-        if (n == 1) return nums[0];
-
-        vector<int> dp1(n, -1);
-        vector<int> dp2(n, -1);
-
-        int case1 = solve(0, n - 2, nums, dp1);
-        int case2 = solve(1, n - 1, nums, dp2);
-
-        return max(case1, case2);
+        return max(robLinear(nums, 0, n-2),
+                   robLinear(nums, 1, n-1));
     }
 
-    int solve(int i, int end, vector<int>& nums, vector<int>& dp) {
-        if (i > end) return 0;
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev1 = 0; // dp[i+1]
+        int prev2 = 0; // dp[i+2]
 
-        if (dp[i] != -1) return dp[i];
+        for(int i = end; i >= start; i--) {
+            int curr = max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
 
-        int pick = nums[i] + solve(i + 2, end, nums, dp);
-        int notPick = solve(i + 1, end, nums, dp);
-
-        return dp[i] = max(pick, notPick);
+        return prev1;
     }
 };
