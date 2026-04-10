71a7cf589cb3a2eb0e349f76ed2c882bece196b1
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-04-10 10:58:20 +0530

    Time: 0 ms (100%), Space: 13.4 MB (38.15%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        
+        vector<vector<int>> dp = triangle; // copy last row automatically
+
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                dp[i][j] += min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
