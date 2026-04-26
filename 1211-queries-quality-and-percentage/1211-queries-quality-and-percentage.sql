c757f10e22d646d9a25ac956aed819f63b612feb
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-04-27 00:11:27 +0530

    Time: 357 ms (50.44%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 select query_name ,
-round(avg(q.rating/q.position),2) as quality ,
-round(sum(if(q.rating < 3 ,1,0))/count(*) * 100 ,2) as poor_query_percentage 
-from Queries q
+round(avg(rating/position),2) as quality ,
+round(sum(if(rating < 3 ,1,0))/count(*) * 100 ,2) as poor_query_percentage 
+from Queries
 group by query_name 
 
