4f4de3d9a874616ff1e1b928103873a71eddc73f
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-04-27 12:37:08 +0530

    Time: 769 ms (13.27%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -7,3 +7,20 @@ select player_id , min(event_date)
 from Activity
 group by player_id
 )
+
+
+
+
+-- SELECT 
+-- ROUND(
+--     COUNT(DISTINCT a.player_id) / 
+--     (SELECT COUNT(DISTINCT player_id) FROM Activity)
+-- , 2) AS fraction
+-- FROM Activity a
+-- JOIN (
+--     SELECT player_id, MIN(event_date) AS first_login
+--     FROM Activity
+--     GROUP BY player_id
+-- ) f
+-- ON a.player_id = f.player_id
+-- AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
