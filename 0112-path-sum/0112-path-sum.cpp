bfbd6c5b1fde2f35bfe456a13b780763d241c5c2
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-05-02 13:55:09 +0530

    Time: 0 ms (100%), Space: 21.6 MB (34.55%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -6,20 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root)
-            return false;
-        if (!root->left && !root->right) {
-            return targetSum == root->val;
+
+        if(!root) return false;
+        stack<pair<TreeNode* ,int>> st;
+        st.push({root,root->val});
+
+        while(!st.empty()){
+            auto top = st.top();
+            TreeNode* node = top.first;
+            int currsum = top.second;
+
+            st.pop();
+
+            if(!node->left && !node->right && currsum == targetSum){
+                return true;
+            }
+            if(node->right){
+                st.push({node->right,node->right->val+currsum});
+            }
+            if(node->left){
+                st.push({node->left,node->left->val+currsum});
+            }
+
         }
-        int remaining = targetSum - root->val;
-        return hasPathSum(root->left, remaining) ||
-               hasPathSum(root->right, remaining);
+        return false;
     }
 };
