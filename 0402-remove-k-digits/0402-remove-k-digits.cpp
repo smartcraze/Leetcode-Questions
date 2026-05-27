427cb988471a2ecd682930c7ba5a70a4d941677a
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-05-27 22:58:31 +0530

    Time: 13 ms (6.72%), Space: 14.1 MB (5.83%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string st;
+        stack<int> st;
 
-        // Step 1: Greedy removal using monotonic stack idea
-        for (char c : num) {
-            while (!st.empty() && k > 0 && st.back() > c) {
-                st.pop_back();
+        for (int i = 0; i < num.size(); i++) {
+            while(!st.empty() && st.top() > num[i] && k >0){
+                st.pop();
                 k--;
             }
-            st.push_back(c);
+            st.push(num[i]);
         }
 
-        // Step 2: If k still left, remove from the end
         while (k > 0 && !st.empty()) {
-            st.pop_back();
+            st.pop();
             k--;
         }
 
-        // Step 3: Remove leading zeros
-        int idx = 0;
-        while (idx < st.size() && st[idx] == '0') idx++;
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
 
-        string ans = st.substr(idx);
+        int i = 0;
+        while (i < ans.size() && ans[i] == '0') {
+            i++;
+        }
 
-        // Step 4: Edge case
-        return ans.empty() ? "0" : ans;
+        ans = ans.substr(i);
+        if (ans == "") return "0";
+        return ans;
     }
 };
