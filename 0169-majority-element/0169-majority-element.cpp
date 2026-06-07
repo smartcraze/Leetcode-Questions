a2172539a460306b27548a10e67a7ee11a717106
Suraj Vishwakarma <dev.surajv@gmail.com>
2026-06-07 14:21:36 +0530

    Time: 0 ms (100%), Space: 28.2 MB (27.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // voting algo 
-        int cnt = 0;
         int candidate = nums[0];
-
-        for(int i=0;i<nums.size();i++){
-            if(cnt==0){
-                candidate = nums[i];
+        int count = 0;
+        for(int num :  nums){
+            if(count == 0){
+                candidate = num;
             }
-            if(nums[i]==candidate) cnt++;
-            else cnt--;
+            if(candidate == num){
+                count++;
+            }else{
+                count--;
+            }
         }
         return candidate;
-        
     }
 };
